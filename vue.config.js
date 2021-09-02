c4028958fe4601175a90fa2cecd4a7d096185925
Radosław Szamszur <rszamszur@adva.com>
2021-09-03 01:20:34 +0200

    Parametrize public path to be taken from .env, add .env

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/pdf-resume-builder/'
+    ? process.env.VUE_APP_PUBLIC_PATH
     : '/',
   transpileDependencies: [
     'vuetify'
